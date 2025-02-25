bb9712dcedd3650d2d9c3d25a6290d64a2fafab7
Ken Thompson <kenthompson1@gmail.com>
2025-02-25 14:21:32 -0500

    tweaks and fixes

--- configs/myprintnc/qtvcp/rapidchange_handler.py
+++ configs/myprintnc/qtvcp/rapidchange_handler.py
@@ -31,7 +31,7 @@ import hal
 #import emccanon
 
 # 5678 is the default attach port in the VS Code debug configurations. Unless a host and port are specified, host defaults to 127.0.0.1
-debugpy.listen(('0.0.0.0',5678))
+#debugpy.listen(('0.0.0.0',5678))
 #print("Waiting for debugger attach")
 #debugpy.wait_for_client()
 #debugpy.breakpoint()
